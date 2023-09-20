105d73c618fc28234345c9e2aa393f8d77e968a1
Harsha Vardhini <109351480+harshashu@users.noreply.github.com>
2023-09-20 14:50:06 +0530

    Update HelloWorldControllerTest.java

--- src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
+++ src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
@@ -20,8 +20,8 @@ public class HelloWorldControllerTest {
     @Test
     public void shouldReturnExpectedMessage() throws Exception {
 
-        mockMvc.perform(get("/hello"))
+        mockMvc.perform(get("/hello3"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello, World!"));
+                .andExpect(content().string("Hello, World!3"));
     }
 }
